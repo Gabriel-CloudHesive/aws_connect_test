6ec38f267935ecbbd933913c76485d20e16f6a55
Gabriel Rodriguez <gabriel.rodriguez@cloudhesive.com>
2023-08-31 13:35:05 -0400

    aws_connect_api package done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:aws_connect_api/connect-2017-08-08.dart';
-import 'package:shared_aws_api/shared.dart' as s;
 
 void main() {
   runApp(const MainApp());
@@ -15,7 +14,10 @@ class MainApp extends StatelessWidget {
       // debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: ElevatedButton(onPressed: myAction, child: const Text('Test')),
+          child: ElevatedButton(
+            onPressed: myAction,
+            child: const Text('Test'),
+          ),
         ),
       ),
     );
@@ -26,24 +28,28 @@ class MainApp extends StatelessWidget {
       region: 'ap-southeast-1',
       endpointUrl: "https://connect.ap-southeast-1.amazonaws.com",
       credentials: AwsClientCredentials(
-        accessKey: "ASIASJ3TVY5DEKLQMIH5",
-        secretKey: "KN+1eUjIlEiB4QYDRLSyU5wx0rMA9Of/O9SU+C7C",
+        accessKey: "ASIASJ3TVY5DLZ6EGNZL",
+        secretKey: "lU2LxzFyqREfk2rW6/0/ePMoQefzGQQWRZc3t6kc",
         sessionToken:
-            "IQoJb3JpZ2luX2VjEPr//////////wEaCXVzLWVhc3QtMSJHMEUCIEN2ft/mXgTzDB9o6ZLw6C1UfuJoISmdq87TSypRbjKLAiEAo382Zgd3BUiu07vpTOAcrRxWyGrDe8eJPEbOjEtuP74qqgMIw///////////ARABGgwxNTg2MTk1MTg3OTAiDM78e0yrA9xo+ANvgyr+Aj5CKiGDRXcAX4brQqkkhyv4Ha4exJwb70rGvC3sRUPTrmb+hyrvyPdYxDkvVPhsWH5tNQxYN1VD/j6M662tS7udg5wDXOGDw6hqZtkSy5qiOBovGB75LyvZ6dgZ+v6qcESNJ3qBHtYBUew/1Llu07wVyOERa6WZuqARg/X3rUWa8t+GWueIft8rek98BA0zNw1d0GjAxdvTu7IiR3ygVW5V+7J9QjNOVAvgMW4BHCnw6T3P7Jxpg7PpCT7u79/feaHb49cjoEaiv4ZCEUOcQQwgVmKxhVphvoxnqO+gCyW9IzeGa+XbqFCvzVumwGQvv2YvWWfce2+gFGBbNjt5+XUrQ3IJd8qI/W+pF7E7pqQqagxSq6LgVatBtR5W3DkFvNPkVOYm76XPIViwSTasy1c9jUxzdS55DpMzvDKXSXhDWImqOTY41/WNDUccDb6CzhlZQmeJLl7ym5ZPZJa01suKgaeOfzD9ggizsfqYfiTotdY7uOAFCjK2dQplowYw34K+pwY6pgH7jCBxl4b7qBUhO94zS+N7ILHnqndejKjPL7uo+8DysO6KaHzvXwvGsHRnroUAHShQyQ1O68uYEV1O5MzZ5fhHdiBnOZDTvh/OAOMLi1MIvscUI1h4Qex+4k37xRA9fSFsh93q81Ms6Zmcc245m9h4MPf7VLZqzHGuqFTHsYTryM8cWoJCure5EPIdSS5mVUPHGRiscpi4quXrKz/KFrzA2ShlvPxg",
+            "IQoJb3JpZ2luX2VjEA4aCXVzLWVhc3QtMSJGMEQCIHikqfcRQrQZmik5TBedrfLDZaQpR9HFQzoeyZVIvzECAiAEIP1TSEwC4zm7+gJu92R3idWS8YnKred+MBqB4ta+iSqqAwjX//////////8BEAEaDDE1ODYxOTUxODc5MCIM/fiT0IIeyoAUFCQ0Kv4CqPRmgggxJ4OuWHOJ4voF4b2n6dvwz/CcF9cUfmu0I2g17LVA5wa+b0U+rT+Zt3IyZypLi3b3bPTpiTTZCg9DJ8mspOwlNitR+s12OIXbqlGPPdM2WXdCo6g00m++l3eiS6/qqsVBrdtOYNLU2IJ6Jvr5NHs1OrEvCHQxPadhBxHpA77KR4p6/E19TB6jpq89uPaXL+HbKa+6heaZafBv0d+1T031SrEf/l0jiolcTn0jd6cND770nQWKJNgijx147ynyTBDN1jaUQB7XKuH3HKSCeGDzzBeheSZZ6AHc+GWUJGQIRnTKGEHM5enMoO9AsU1dsYgTKO14mEBl1+JxDMksORPuBqmIDTi4yHSHq8ySkOL+ES24J/qt+YWGCi/7ekKw716HcB1e+TTrM3aHS2j+Vo0hfDM4KEW4fj8upsO/Ax9aQspQcn4gUwMZoS5V3W5wtZ7IbA+NE5802iTTE6cbbG1hlJ620JjmQEIkxGYCU7bVbouec63H9sjpGzC2qMKnBjqnAeqWllNJz4dW7DQWNoEfrYFhnj9hcH88bdX5+qdFh/T072jc5HBEabCkL7zs5a18j/JWIGO0Jbo5W1U1m8dvqeD12+twD7YsqZWJmXtM0bgLrB7A+ud5vvL3dB++vKysINwV4k7cxEiDXZ56AMVRuPSxySb29vS+EPJDUDD8Z6V+eYEuyu0JJqO/TIMBdS8LIeiaK8OLDkrIFFZSxu+kYqYHoHRhKDjT",
       ),
     );
 
-    ListInstancesResponse inst = await service.listInstances();
-    List<InstanceSummary>? summaryList = inst.instanceSummaryList;
-    InstanceSummary sum = summaryList!.elementAt(0);
-    service.createQuickConnect(
-      instanceId: sum.id!,
-      name: "QCTest!!!",
-      description: "Description test.",
-      quickConnectConfig: QuickConnectConfig(
-        quickConnectType: QuickConnectType.phoneNumber,
-        phoneConfig: PhoneNumberQuickConnectConfig(phoneNumber: "+13054912981"),
-      ),
-    );
+    ListInstancesResponse instancesResponse = await service.listInstances();
+    List<InstanceSummary>? instacesList = instancesResponse.instanceSummaryList;
+    InstanceSummary instance = instacesList!.elementAt(0);
+
+    ListContactFlowsResponse contactFlowsResponse =
+        await service.listContactFlows(instanceId: instance.id!);
+    List<ContactFlowSummary>? contactFlowsList =
+        contactFlowsResponse.contactFlowSummaryList;
+    ContactFlowSummary contatcFlow = contactFlowsList!.elementAt(0);
+
+    StartOutboundVoiceContactResponse start =
+        await service.startOutboundVoiceContact(
+            contactFlowId: "5974d26c-868f-462c-9a72-7f196858bfbb",
+            destinationPhoneNumber: "+13054912981",
+            instanceId: instance.id!,
+            queueId: "766f3755-ac91-4d4d-81d2-f1374e69a206");
   }
 }
